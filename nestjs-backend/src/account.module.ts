993e7898238bb2fcfc65c61ea22f788f8f872729
ngoctuan1105 <151895773+ngoctuan1105@users.noreply.github.com>
2026-09-23 11:29:37 +0700

    tạo CRUD cho Account

---
+++ nestjs-backend/src/account.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AccountController } from './account.controller';
+import { AccountService } from './account.service';
+import { accountProviders } from './account.provider';
+import { DatabaseModule } from './database.module';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [AccountController],
+  providers: [...accountProviders, AccountService],
+})
+export class AccountModule {}
