4460f53146677cfca0f71e664d1d08b2d4fe9922
YukiMitsuki <140571908+YukiMitsuki@users.noreply.github.com>
2026-09-23 11:03:44 +0700

    Tạo Entity

--- src/user.entity.ts
+++ src/user.entity.ts
@@ -1,1 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
 
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  UserId: string;
+
+  @Column({ length: 50, unique: true })
+  Username: string;
+
+  @Column()
+  PasswordHash: string;
+
+  @Column({ length: 100, unique: true })
+  Email: string;
+
+  @Column({ length: 100 })
+  FullName: string;
+
+  @CreateDateColumn()
+  CreatedAt: Date;
+}
