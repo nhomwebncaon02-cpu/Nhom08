5534f002b030420f3d2fa289d1145e1785727deb
YukiMitsuki <140571908+YukiMitsuki@users.noreply.github.com>
2026-09-23 11:34:59 +0700

    API cho CRUD user

--- nestjs-backend/src/user.controller.ts
+++ nestjs-backend/src/user.controller.ts
@@ -1,1 +1,33 @@
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
+@Controller('users') 
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() userData: Partial<User>) {
+    return this.userService.create(userData);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateData: Partial<User>) {
+    return this.userService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(id);
+  }
+}
