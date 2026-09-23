df4ed773b9f680d7353c75dabf3019fb04a7a67a
YukiMitsuki <140571908+YukiMitsuki@users.noreply.github.com>
2026-09-23 11:32:20 +0700

    Tạo CRUD cho user

--- nestjs-backend/src/user.service.ts
+++ nestjs-backend/src/user.service.ts
@@ -1,1 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(userData: Partial<User>): Promise<User> {
+    const newUser = this.userRepository.create(userData);
+    return await this.userRepository.save(newUser);
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: string): Promise<User | null> {
+    return await this.userRepository.findOne({ where: { UserId: id } });
+  }
+
+  async update(id: string, updateData: Partial<User>): Promise<User | null> {
+    await this.userRepository.update(id, updateData);
+    return this.findOne(id);
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
